e142232c8fc667a02f2055b59fcf3707591e2b5e
Shawn Jacobson <146896275+shawnjacobson33@users.noreply.github.com>
2025-01-24 23:19:53 -0600

    More login and signup functionality is added.

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -1,21 +1,81 @@
+'use client'
+
 import React from 'react'
+import { useRouter } from 'next/navigation';
 
 function LogIn() {
+
+    const router = useRouter()
+
+    const [formData, setFormData] = React.useState({
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (event: { target: { name: string, value: string } }) => {
+        const { name, value } = event.target;
+        setFormData({...formData, [name]: value})
+    }
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault()
+
+        try {
+            const response = await fetch('http://localhost:8080/users/auth', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData)
+            })
+
+            if (response.ok) {
+                const { token, user } = await response.json()
+
+                document.cookie = `token=${token}; max-age=86400; Secure; HttpOnly; SameSite=Strict`;
+                localStorage.setItem('user', JSON.stringify(user))
+
+                router.push('/value-board')
+
+            } else {
+                alert("Invalid Credentials!")
+            }
+
+        } catch (e) {
+            console.log("Error:", e)
+        }
+
+    }
+
     return (
         <div className="flex flex-col items-center justify-center space-y-3 h-screen bg-black">
-            <div className="border mb-[8rem] p-[3rem] bg-red-500">
+            <div className="border border-red-500 shadow-md shadow-red-500 mb-[8rem] p-[3rem] rounded">
                 <div className="font-bold text-3xl text-white">Welcome Back!</div>
             </div>
-            <input
-                type='text'
-                name='email'
-                placeholder="Email Address"
-                className="p-4 w-[20rem] rounded"/>
-            <input
-                type='text'
-                name='password'
-                placeholder="Password"
-                className="p-4 w-[20rem] rounded"/>
+            <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+                <input
+                    type='email'
+                    name='email'
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="Email Address"
+                    className="input"
+                    required
+                />
+                <input
+                    type='password'
+                    name='password'
+                    value={formData.password}
+                    onChange={handleChange}
+                    placeholder="Password"
+                    className="input"
+                    required
+                />
+                <button
+                    type='submit'
+                    className="input"
+                >
+                    Submit
+                </button>
+            </form>
 
         </div>
     )
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,27 +1,84 @@
+'use client'
+
 import React from 'react'
 
 function SignUp() {
+
+    const [formInput, setFormInput] = React.useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const handleOnChange = (e: { target: { name: string; value: string; }; }) => {
+        const { name, value } = e.target;
+        setFormInput({...formInput, [name]: value})
+    }
+
+    const handleOnSubmit = async (e: { preventDefault: () => void; })  => {
+        try {
+            e.preventDefault()
+
+            const response = await fetch('http://localhost:8080/users/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formInput)
+            })
+
+            if (!response.ok) {
+                throw new Error("network error!")
+            }
+
+            const serverData = await response.json()
+
+            console.log("Server Response:", serverData)
+
+        } catch (e) {
+            console.log("Error: ", e)
+        }
+
+    }
+
     return (
         <div className="flex flex-col items-center justify-center space-y-3 h-screen bg-black">
-            <div className="border mb-[8rem] p-[3rem] bg-red-500">
+            <div className="border border-red-500 shadow-md shadow-red-500 mb-[8rem] p-[3rem] rounded">
                 <div className="font-bold text-3xl text-white">Create an Account</div>
             </div>
-            <input
-                type='text'
-                name='username'
-                placeholder="Username"
-                className="p-4 w-[20rem] rounded"/>
-            <input
-                type='text'
-                name='email'
-                placeholder="Email Address"
-                className="p-4 w-[20rem] rounded"/>
-            <input
-                type='text'
-                name='password'
-                placeholder="Password"
-                className="p-4 w-[20rem] rounded"/>
-
+            <form className="flex flex-col gap-4" onSubmit={handleOnSubmit}>
+                <input
+                    type='text'
+                    name='username'
+                    value={formInput.username}
+                    onChange={handleOnChange}
+                    placeholder="Username"
+                    className="input"
+                    required
+                />
+                <input
+                    type='email'
+                    name='email'
+                    value={formInput.email}
+                    onChange={handleOnChange}
+                    placeholder="Email Address"
+                    className="input"
+                    required
+                />
+                <input
+                    type='password'
+                    name='password'
+                    value={formInput.password}
+                    onChange={handleOnChange}
+                    placeholder="Password"
+                    className="input"
+                    required
+                />
+                <button
+                    type='submit'
+                    className="input"
+                >
+                    Submit
+                </button>
+            </form>
         </div>
     )
 }
